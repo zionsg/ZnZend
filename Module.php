ed2842dbab4bea44d607f8284a473e434afa3501
Zion Ng <zion@intzone.com>
2013-04-02 13:00:13 +0800

    Add comments on use of 'initializer' key.

--- Module.php
+++ Module.php
@@ -28,9 +28,13 @@ class Module
     public function getServiceConfig()
     {
         return array(
+            // Any object pulled from a service manager is ran through the registered initializers
             'initializers' => array(
                 function ($instance, $sm) {
                     // Sets default db adapter
+                    // Instance needs to implement the interface and be pulled from a service manager for this to work
+                    // Example in controller action:
+                    //     $userTable = $this->getServiceLocator()->get('Application\Model\UserTable');
                     if ($instance instanceof \Zend\Db\Adapter\AdapterAwareInterface) {
                         $instance->setDbAdapter($sm->get('Zend\Db\Adapter\Adapter'));
                     }
