29869de4e09e34baf24cd3fe0b2b70cd499c0f8e
Zion Ng <zion@intzone.com>
2013-07-05 16:30:04 +0800

    Update to include Zend\Form\Annotation and entity properties

--- src/ZnZend/Db/Generator/EntityGenerator.php
+++ src/ZnZend/Db/Generator/EntityGenerator.php
@@ -13,15 +13,17 @@ use Zend\Code\Generator\ClassGenerator;
 use Zend\Code\Generator\DocBlockGenerator;
 use Zend\Code\Generator\PropertyGenerator;
 use Zend\Code\Generator\MethodGenerator;
+use Zend\Code\Generator\ParameterGenerator;
+use Zend\Code\Generator\DocBlock\Tag;
+use Zend\Code\Generator\DocBlock\Tag\ParamTag;
 use Zend\Code\Generator\DocBlock\Tag\ReturnTag;
-use ZnZend\Db\AbstractEntity;
 use ZnZend\Db\Exception;
 
 /**
  * Generate entity classes for tables in a database based on AbstractEntity
  *
  * This only generates the initial classes and does not do all the work for you.
- * The entity name, $mapGettersColumns and getters for each column are generated
+ * The entity name, $_mapGettersColumns, getters and setters for each column are generated
  * using simplistic naming rules. Eg: For a `useragent` column, the generated getter
  * will be named 'getUseragent()' and not 'getUserAgent()'.
  */
@@ -49,7 +51,6 @@ class EntityGenerator
         'timestamp' => 'DateTime',
     );
 
-
     /**
      * Generate entity classes for all tables in active database
      *
@@ -59,8 +60,9 @@ class EntityGenerator
      * @param string   $namespace           Namespace for entity and table gateway classes
      * @param callable $columnToGetterFunc  Optional callback that takes in (string $tableName, string $columnName)
      *                                      and returns getter name
-     * @param DocBlockGenerator $fileDocBlock   Optional docblock for all files
-     * @param DocBlockGenerator $entityDocBlock Optional docblock for all entity classes.
+     * @param callable $columnToSetterFunc  Optional callback that takes in (string $tableName, string $columnName)
+     *                                      and returns setter name
+     * @param DocBlockGenerator $fileDocBlock Optional docblock for all files
      * @throws Exception\InvalidArgumentException When path is not writable
      * @return void
      */
@@ -69,26 +71,18 @@ class EntityGenerator
         \Zend\Db\Adapter\Adapter $dbAdapter,
         $namespace,
         $columnToGetterFunc = null,
+        $columnToSetterFunc = null,
         DocBlockGenerator $fileDocBlock = null,
         DocBlockGenerator $entityDocBlock = null
     ) {
         if (!is_writable($filePath)) {
             throw new Exception\InvalidArgumentException("{$filePath} is not writable");
         }
-
-        // Default callback for generating getter name from column name
-        // Eg: 'name' and 'person_name' becomes 'getName'
-        // Eg: 'isdeleted' and 'person_isdeleted' becomes 'isDeleted'
         if (!is_callable($columnToGetterFunc)) {
-            $columnToGetterFunc = function ($tableName, $columnName) {
-                $pattern = '/^.*[^a-zA-Z0-9]+(.+)$/';
-                $matches = array();
-                $normalizedName = (preg_match($pattern, $columnName, $matches) ? $matches[1] : $columnName);
-                if (0 === stripos($normalizedName, 'is')) {
-                    return 'is' . ucfirst(substr($normalizedName, 2));
-                }
-                return 'get' . ucfirst($normalizedName);
-            };
+            $columnToGetterFunc = self::columnToGetterFunc();
+        }
+        if (!is_callable($columnToSetterFunc)) {
+            $columnToSetterFunc = self::columnToSetterFunc();
         }
 
         // Iterate thru tables
@@ -108,54 +102,141 @@ class EntityGenerator
                 array($databaseName, $tableName)
             );
 
-            // Create getter methods for each column and map them
+            // Create getter and setters for each column and map them
             $mapGettersColumns = array();
+            $properties = array();
             $methods = array();
             $types = array();
             foreach ($columns as $column) {
                 $columnName = $column->column_name;
-                $dataType   = $column->data_type;
+                $sqlType    = $column->data_type;
+                $phpType    = self::getPhpType($sqlType);
                 $getterName = $columnToGetterFunc($tableName, $columnName);
-                if (!$getterName) {
-                    continue;
-                }
-                $mapGettersColumns[$getterName] = $columnName;
+                $setterName = $columnToSetterFunc($tableName, $columnName);
+                $label = substr(
+                    $getterName ?: $setterName,
+                    strcspn($getterName ?: $setterName, 'ABCDEFGHJIJKLMNOPQRSTUVWXYZ')
+                );
 
-                $returnType = self::getReturnType($dataType);
-                $desc = 'Get ' . lcfirst(substr($getterName, strcspn($getterName, 'ABCDEFGHJIJKLMNOPQRSTUVWXYZ')));
-                $methods[] = MethodGenerator::fromArray(array(
-                    'name'       => $getterName,
-                    'body'       => 'return ' . self::getTypeCast('$this->get()', $dataType),
+                $properties[] = PropertyGenerator::fromArray(array(
+                    'name'       => $columnName,
+                    'visibility' => 'protected',
                     'docblock'   => DocBlockGenerator::fromArray(array(
-                        'shortDescription' => $desc,
+                        'shortDescription' => null,
                         'longDescription'  => null,
                         'tags'             => array(
-                            new ReturnTag(array(
-                                'datatype'  => $returnType,
+                            new Tag(array(
+                                'name' => '@Annotation\Name("' . $columnName . '")',
+                            )),
+                            new Tag(array(
+                                'name' => '@Annotation\Filter({"name":"StringTrim"})',
+                            )),
+                            new Tag(array(
+                                'name' => '@Annotation\Required(false)',
+                            )),
+                            new Tag(array(
+                                'name' => '@Annotation\Type("Zend\Form\Element\Text")',
+                            )),
+                            new Tag(array(
+                                'name' => '@Annotation\Attributes({"placeholder": "' . $label . '"})',
+                            )),
+                            new Tag(array(
+                                'name' => '@Annotation\Options({"label": "' . $label . '"})',
                             )),
                         ),
                     )),
                 ));
+
+                if ($getterName) {
+                    $mapGettersColumns[$getterName] = $columnName;
+
+                    $desc = 'Get ' . lcfirst($label);
+                    $methods[] = MethodGenerator::fromArray(array(
+                        'name'       => $getterName,
+                        'body'       =>   ('string' == $phpType)
+                                        ? 'return $this->get();'
+                                        : 'return ' . self::getTypeCast('$this->get()', $sqlType),
+                        'docblock'   => DocBlockGenerator::fromArray(array(
+                            'shortDescription' => $desc,
+                            'longDescription'  => null,
+                            'tags'             => array(
+                                new ReturnTag(array(
+                                    'datatype'  => self::getPhpType($sqlType),
+                                )),
+                            ),
+                        )),
+                    ));
+                }
+
+                if ($setterName) {
+                    $desc = 'Set ' . lcfirst($label);
+                    $methods[] = MethodGenerator::fromArray(array(
+                        'name'       => $setterName,
+                        'parameters' => array(
+                            ParameterGenerator::fromArray(array('name' => 'value')),
+                        ),
+                        'body'       =>   ('string' == $phpType)
+                                        ? 'return $this->set($value);'
+                                        : "return \$this->set(\$value, '{$phpType}');",
+                        'docblock'   => DocBlockGenerator::fromArray(array(
+                            'shortDescription' => $desc,
+                            'longDescription'  => null,
+                            'tags'             => array(
+                                new ParamTag(array(
+                                    'paramName' => 'value',
+                                    'datatype'  => 'null|' . $phpType,
+                                )),
+                                new ReturnTag(array(
+                                    'datatype'  => $entityName,
+                                )),
+                            ),
+                        )),
+                    ));
+                }
             }
 
             // Generate class
             $entityClass = new ClassGenerator();
             $entityClass->setName($entityName)
                         ->setNamespaceName($namespace)
+                        ->addUse('Zend\Form\Annotation')
                         ->addUse('ZnZend\Db\AbstractEntity')
-                        ->setExtendedClass('AbstractEntity');
-            if ($entityDocBlock !== null) {
-                $entityClass->setDocBlock($entityDocBlock);
-            }
+                        ->setExtendedClass('AbstractEntity')
+                        ->setDocBlock(DocBlockGenerator::fromArray(array(
+                              'shortDescription' => null,
+                              'longDescription'  => null,
+                              'tags'             => array(
+                                  new Tag(array(
+                                      'name' => '@Annotation\Name("' . $entityName . '")',
+                                  )),
+                                  new Tag(array(
+                                      'name' => '@Annotation\Hydrator("Zend\Stdlib\Hydrator\ArraySerializable")',
+                                  )),
+                              ),
+                          )));
             if ($mapGettersColumns) {
-                $entityClass->addProperties(array(
-                    PropertyGenerator::fromArray(array(
-                        'name'         => 'mapGettersColumns',
-                        'visibility'   => 'protected',
-                        'static'       => true,
-                        'defaultValue' => $mapGettersColumns,
+                $properties[]= PropertyGenerator::fromArray(array(
+                    'name'         => '_mapGettersColumns',
+                    'visibility'   => 'protected',
+                    'static'       => true,
+                    'defaultValue' => $mapGettersColumns,
+                    'docblock'     => DocBlockGenerator::fromArray(array(
+                        'shortDescription' => null,
+                        'longDescription'  => null,
+                        'tags'             => array(
+                            new Tag(array(
+                                'name' => '@Annotation\Exclude()',
+                            )),
+                            new Tag(array(
+                                'name' => '@var',
+                                'description' => 'array',
+                            )),
+                        ),
                     ))
                 ));
+            }
+            if ($properties) {
+                $entityClass->addProperties($properties);
             }
             if ($methods) {
                 $entityClass->addMethods($methods);
@@ -173,21 +254,65 @@ class EntityGenerator
             // 1) Only 1 blank line between file docblock and namespace
             // 2) No blank line between opening brace for class and first property
             // 3) No blank line between last method and closing brace for class
+            // 4) Closing bracket for $_mapGettersColumns array property should not be indented
             $output = $entityFile->generate();
             $output = str_replace("\nnamespace", "namespace", $output);
             $output = str_replace("{\n\n", "{\n", $output);
             $output = str_replace("\n\n}\n\n", "}\n", $output);
+            $output = preg_replace('/[ ]{4}([ ]{4}\);)/', '\1', $output);
             file_put_contents("{$filePath}/{$entityName}.php", $output);
         }
     }
 
     /**
-     * Get return type for use in docblock
+     * Get default callback for generating getter name from column name
+     *
+     * 'name' and 'person_name' becomes 'getName'
+     * 'isdeleted' and 'person_isdeleted' becomes 'isDeleted'
+     *
+     * @return callable
+     */
+    protected static function columnToGetterFunc()
+    {
+        return function ($tableName, $columnName) {
+            $pattern = '/^.*[^a-zA-Z0-9]+(.+)$/';
+            $matches = array();
+            $normalizedName = (preg_match($pattern, $columnName, $matches) ? $matches[1] : $columnName);
+            if (0 === stripos($normalizedName, 'is')) {
+                return 'is' . ucfirst(substr($normalizedName, 2));
+            }
+            return 'get' . ucfirst($normalizedName);
+        };
+    }
+
+    /**
+     * Get default callback for generating setter name from column name
+     *
+     * 'name' and 'person_name' becomes 'setName'
+     * 'isdeleted' and 'person_isdeleted' becomes 'setDeleted'
+     *
+     * @return callable
+     */
+    protected static function columnToSetterFunc()
+    {
+        return function ($tableName, $columnName) {
+            $pattern = '/^.*[^a-zA-Z0-9]+(.+)$/';
+            $matches = array();
+            $normalizedName = (preg_match($pattern, $columnName, $matches) ? $matches[1] : $columnName);
+            if (0 === stripos($normalizedName, 'is')) {
+                return 'set' . ucfirst(substr($normalizedName, 2));
+            }
+            return 'set' . ucfirst($normalizedName);
+        };
+    }
+
+    /**
+     * Get PHP data type for use in docblock
      *
      * @param  string $sqlType SQL data type
      * @return string
      */
-    protected static function getReturnType($sqlType)
+    protected static function getPhpType($sqlType)
     {
         return (isset(self::$mapTypes[$sqlType]) ? self::$mapTypes[$sqlType] : 'string');
     }
@@ -203,12 +328,12 @@ class EntityGenerator
      */
     protected static function getTypeCast($expr, $sqlType)
     {
-        $returnType = self::getReturnType($sqlType);
-        if ($returnType == strtolower($returnType)) {
-            return "({$returnType}) {$expr};";
+        $phpType = self::getPhpType($sqlType);
+        if ($phpType == strtolower($phpType)) {
+            return "({$phpType}) {$expr};";
         }
 
         // Cast to object
-        return "new {$returnType}({$expr});";
+        return "new {$phpType}({$expr});";
     }
 }
