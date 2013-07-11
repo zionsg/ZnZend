9a06b85bf3cfa0d4df1657cb8ddddee426b7faaf
Zion Ng <zion@intzone.com>
2013-07-11 10:12:43 +0800

    Rename addResourceRecursive to addResource

--- src/ZnZend/Permissions/Acl/Acl.php
+++ src/ZnZend/Permissions/Acl/Acl.php
@@ -16,31 +16,48 @@ use ZnZend\Permissions\Acl\Exception;
  * Custom acl class
  *
  * Additions to ZendAcl:
- *   - addResourceRecursive() method created for adding resource ids that follow
+ *   - addResource() method modified to recursively add resource ids that follow
  *     SNMP Object Id human-readable naming style, eg. root.parent.child
  */
 class Acl extends ZendAcl
 {
     /**
-     * Adds a Resource and its parents recursively
+     * Defined by ZendAcl; Adds a Resource having an identifier unique to the ACL
      *
-     * This is for adding resource ids that follow SNMP Object Id naming style, eg. root.parent.child
-     * For a resource id such as 'root.parent.child', this does the following:
+     * This recursively adds resource ids that follow SNMP Object Id naming style, eg. root.parent.child
+     * Adding a resource id such as 'root.parent.child' will do the following:
      *   addResource('root.parent.child', 'root.parent');
      *   addResource('root.parent', 'root');
      *
      * @param  Resource\ResourceInterface|string $resource
+     * @param  Resource\ResourceInterface|string $parent
      * @throws Exception\InvalidArgumentException
      * @return Acl Provides a fluent interface
      */
-    public function addResourceRecursive($resource)
+    public function addResource($resource, $parent = null)
     {
         if (is_string($resource)) {
             $resource = new Resource\GenericResource($resource);
         } elseif (!$resource instanceof Resource\ResourceInterface) {
             throw new Exception\InvalidArgumentException(
-                'addResourceRecursive() expects $resource to be of type Zend\Permissions\Acl\Resource\ResourceInterface'
+                'addResource() expects $resource to be of type Zend\Permissions\Acl\Resource\ResourceInterface'
             );
+        }
+        if ($this->hasResource($resource->getResourceId())) {
+            return $this;
+        }
+
+        if ($parent != null) {
+            if (is_string($parent)) {
+                $parent = new Resource\GenericResource($parent);
+            } elseif (!$parent instanceof Resource\ResourceInterface) {
+                throw new Exception\InvalidArgumentException(
+                    'addResource() expects $parent to be of type Zend\Permissions\Acl\Resource\ResourceInterface'
+                );
+            }
+            if (!$this->hasResource($parent->getResourceId())) {
+                $this->addResource($parent);
+            }
         }
 
         $currentResources = $this->getResources();
@@ -48,7 +65,7 @@ class Acl extends ZendAcl
         $count = count($resourceIds);
 
         $resourceId = $resourceIds[0];
-        $parentResourceId = $resourceIds[0];
+        $parentResourceId = ($parent ? $parent->getResourceId() : null);
 
         for ($i = 0; $i < $count; $i++) {
             if ($i > 0) {
@@ -62,9 +79,9 @@ class Acl extends ZendAcl
 
             if (0 == $i) {
                 // Root
-                $this->addResource($resourceId);
+                parent::addResource($resourceId, $parentResourceId);
             } else {
-                $this->addResource($resourceId, $parentResourceId);
+                parent::addResource($resourceId, $parentResourceId);
                 $parentResourceId = $resourceId;
             }
         }
