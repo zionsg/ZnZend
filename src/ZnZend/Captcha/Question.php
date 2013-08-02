9512a2b4c928fcde2ab3341ed437c7e77ce1c711
Zion Ng <zion@intzone.com>
2013-08-02 17:25:02 +0800

    Use service for generating question

--- src/ZnZend/Captcha/Question.php
+++ src/ZnZend/Captcha/Question.php
@@ -9,7 +9,9 @@
 namespace ZnZend\Captcha;
 
 use Zend\Captcha\AbstractWord;
+use Zend\Session\Container;
 use ZnZend\Captcha\Exception;
+use ZnZend\Captcha\Service\QuestionServiceInterface;
 
 /**
  * Image-based captcha adapter for custom questions and answers
@@ -20,32 +22,24 @@ use ZnZend\Captcha\Exception;
  * Bulk of code from Zend\Captcha\Image with the following differences:
  *   - No need for imgDir as inline image is generated
  *   - Width and height are automatically calculated to fit the text
- *   - Captcha question is supplied, not generated
+ *   - Captcha question is supplied by service, not generated internally
  *   - isValid() checks against supplied answer, not question
  */
 class Question extends AbstractWord
 {
     /**
-     * Question
+     * Service for generating question and verifying answer - must be set
+     *
+     * @var QuestionServiceInterface
+     */
+    protected $service;
+
+    /**
+     * Fully qualified path to image font file - must be set
      *
      * @var string
      */
-    protected $question;
-
-    /**
-     * Answer
-     *
-     * @var string
-     */
-    protected $answer;
-
-    /**
-     * Inline image generated for captcha question
-     *
-     * @example "data:image\/png;base64,iVBORw0KGgoAAAANSUhEUgAA..."
-     * @var string For use in HTML <img src=".." />
-     */
-    protected $inlineImage;
+    protected $font;
 
     /**
      * Font size
@@ -55,13 +49,6 @@ class Question extends AbstractWord
     protected $fontSize = 24;
 
     /**
-     * Fully qualified path to image font file
-     *
-     * @var string
-     */
-    protected $font;
-
-    /**
      * Padding (in pixels) around question in generated image
      *
      * @var int
@@ -69,16 +56,14 @@ class Question extends AbstractWord
     protected $padding = 5;
 
     /**
-     * Number of noise dots on image
-     * Used twice - before and after transform
+     * Percentage of noise dots on image
      *
      * @var int
      */
-    protected $dotNoiseLevel = 100;
+    protected $dotNoisePercent = 10;
 
     /**
      * Number of noise lines on image
-     * Used twice - before and after transform
      *
      * @var int
      */
@@ -100,18 +85,42 @@ class Question extends AbstractWord
     public function __construct($options = null)
     {
         if (!extension_loaded('gd')) {
-            throw new Exception\ExtensionNotLoadedException('Image CAPTCHA requires GD extension');
+            throw new Exception\ExtensionNotLoadedException('Question CAPTCHA requires GD extension');
         }
 
         if (!function_exists("imagepng")) {
-            throw new Exception\ExtensionNotLoadedException('Image CAPTCHA requires PNG support');
+            throw new Exception\ExtensionNotLoadedException('Question CAPTCHA requires PNG support');
         }
 
         if (!function_exists("imageftbbox")) {
-            throw new Exception\ExtensionNotLoadedException('Image CAPTCHA requires FreeType fonts support');
+            throw new Exception\ExtensionNotLoadedException('Question CAPTCHA requires FreeType fonts support');
+        }
+
+        if (!isset($options['font']) || !file_exists($options['font'])) {
+            throw new Exception\NoFontProvidedException('Question CAPTCHA requires font');
         }
 
         parent::__construct($options);
+    }
+
+    /**
+     * Generate captcha
+     *
+     * @return string captcha ID
+     */
+    public function generate()
+    {
+        $id = $this->generateRandomId();
+        $this->setId($id);
+
+        $service = $this->getService();
+        $service->generate();
+
+        $session = $this->getSession();
+        $session->question = $service->getQuestion();
+        $session->image = $this->generateImage();
+
+        return $id;
     }
 
     /**
@@ -131,25 +140,7 @@ class Question extends AbstractWord
      */
     public function getQuestion()
     {
-        if (empty($this->question)) {
-            $session        = $this->getSession();
-            $this->question = $session->question;
-        }
-        return $this->question;
-    }
-
-    /**
-     * Get captcha answer
-     *
-     * @return string
-     */
-    public function getAnswer()
-    {
-        if (empty($this->answer)) {
-            $session      = $this->getSession();
-            $this->answer = $session->answer;
-        }
-        return $this->answer;
+        return $this->getSession()->question;
     }
 
     /**
@@ -157,109 +148,31 @@ class Question extends AbstractWord
      *
      * @return string
      */
-    public function getInlineImage()
+    public function getImage()
     {
-        if (empty($this->inlineImage)) {
-            $session     = $this->getSession();
-            $this->inlineImage = $session->inlineImage;
-        }
-        return $this->inlineImage;
+        return $this->getSession()->image;
     }
 
     /**
-     * Get padding
+     * Set captcha question service
      *
-     * @return int
-     */
-    public function getPadding()
-    {
-        return $this->padding;
-    }
-
-    /**
-     * Get font to use when generating captcha
-     *
-     * @return string
-     */
-    public function getFont()
-    {
-        return $this->font;
-    }
-
-    /**
-     * Get font size
-     *
-     * @return int
-     */
-    public function getFontSize()
-    {
-        return $this->fontSize;
-    }
-
-    /**
-     * @return int
-     */
-    public function getDotNoiseLevel()
-    {
-        return $this->dotNoiseLevel;
-    }
-
-    /**
-     * @return int
-     */
-    public function getLineNoiseLevel()
-    {
-        return $this->lineNoiseLevel;
-    }
-
-    /**
-     * @return bool
-     */
-    public function getTransformImage()
-    {
-        return $this->transformImage;
-    }
-
-    /**
-     * Set captcha question
-     *
-     * @param  string $question
+     * @param  QuestionServiceInterface $service
      * @return Question
      */
-    public function setQuestion($question)
+    public function setService(QuestionServiceInterface $service)
     {
-        $session           = $this->getSession();
-        $session->question = $question;
-        $this->question    = $question;
+        $this->service = $service;
         return $this;
     }
 
     /**
-     * Set captcha answer
+     * Get captcha question service
      *
-     * @param  string $answer
-     * @return Question
+     * @return QuestionServiceInterface
      */
-    public function setAnswer($answer)
+    public function getService()
     {
-        $session         = $this->getSession();
-        $session->answer = $answer;
-        $this->answer    = $answer;
-        return $this;
-    }
-
-    /**
-     * Set inline image generated for captcha question
-     *
-     * @param  string $inlineImage
-     * @return Question
-     */
-    public function setInlineImage($inlineImage)
-    {
-        $session              = $this->getSession();
-        $session->inlineImage = $inlineImage;
-        $this->inlineImage    = $inlineImage;
-        return $this;
+        return $this->service;
     }
 
     /**
@@ -275,6 +188,16 @@ class Question extends AbstractWord
     }
 
     /**
+     * Get font to use when generating captcha
+     *
+     * @return string
+     */
+    public function getFont()
+    {
+        return $this->font;
+    }
+
+    /**
      * Set captcha font size
      *
      * @param  int $fontSize
@@ -284,6 +207,16 @@ class Question extends AbstractWord
     {
         $this->fontSize = $fontSize;
         return $this;
+    }
+
+    /**
+     * Get font size
+     *
+     * @return int
+     */
+    public function getFontSize()
+    {
+        return $this->fontSize;
     }
 
     /**
@@ -299,13 +232,31 @@ class Question extends AbstractWord
     }
 
     /**
-     * @param int $dotNoiseLevel
+     * Get padding
+     *
+     * @return int
+     */
+    public function getPadding()
+    {
+        return $this->padding;
+    }
+
+    /**
+     * @param int $dotNoisePercent
      * @return Question
      */
-    public function setDotNoiseLevel($dotNoiseLevel)
+    public function setDotNoisePercent($dotNoisePercent)
     {
-        $this->dotNoiseLevel = $dotNoiseLevel;
+        $this->dotNoisePercent = $dotNoisePercent;
         return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDotNoisePercent()
+    {
+        return $this->dotNoisePercent;
     }
 
     /**
@@ -318,6 +269,14 @@ class Question extends AbstractWord
         return $this;
     }
 
+    /**
+     * @return int
+     */
+    public function getLineNoiseLevel()
+    {
+        return $this->lineNoiseLevel;
+    }
+
     /*
      * @param bool $transformImage
      * @return Question
@@ -326,6 +285,14 @@ class Question extends AbstractWord
     {
         $this->transformImage = $transformImage;
         return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getTransformImage()
+    {
+        return $this->transformImage;
     }
 
     /**
@@ -360,43 +327,30 @@ class Question extends AbstractWord
     }
 
     /**
-     * Generate captcha
-     *
-     * @return string captcha ID
-     */
-    public function generate()
-    {
-        $id = $this->generateRandomId();
-        $this->setId($id);
-        $this->generateInlineImage($id, $this->getQuestion());
-
-        return $id;
-    }
-
-    /**
-     * Generate inline image captcha for question
+     * Generate inline image for captcha question
      *
      * Image format is in PNG
      * Wave transform from http://www.captcha.ru/captchas/multiwave/
      *
-     * @param  string $id Captcha ID
-     * @param  string $word Captcha word
-     * @return void
+     * @return string
      * @throws Exception\NoFontProvidedException if no font was set
      * @throws Exception\ImageNotLoadableException if start image cannot be loaded
      */
-    protected function generateInlineImage($id, $question)
+    protected function generateImage()
     {
-        $font = $this->getFont();
+        $text     = $this->getQuestion();
+        $font     = $this->getFont();
+        $fontSize = $this->getFontSize();
+        $padding  = $this->getPadding();
+        $dotNoisePercent = $this->getDotNoisePercent();
+        $lineNoiseLevel = $this->getLineNoiseLevel();
 
         if (empty($font)) {
-            throw new Exception\NoFontProvidedException('Image CAPTCHA requires font');
+            throw new Exception\NoFontProvidedException('Question CAPTCHA requires font');
         }
 
         // Retrieve bounding box and calculate text dimensions
-        $fontSize    = $this->getFontSize();
-        $padding     = $this->getPadding();
-        $typeSpace   = imageftbbox($fontSize, 0, $font, $question);
+        $typeSpace   = imagettfbbox($fontSize, 0, $font, $text);
         $width  = abs($typeSpace[4] - $typeSpace[0]) + (2 * $padding);
         $height = abs($typeSpace[5] - $typeSpace[1]) + (2 * $padding);
 
@@ -411,11 +365,12 @@ class Question extends AbstractWord
         // $y is the font baseline, not the very bottom of the character, eg. "g", hence minus (1.5 * padding)
         $x = $padding;
         $y = $height - (1.5 * $padding);
-        imagefttext($image, $fontSize, 0, $x, $y, $textColor, $font, $question);
+        $noisePixels = ($dotNoisePercent / 100) * ($width * $height);
+        imagettftext($image, $fontSize, 0, $x, $y, $textColor, $font, $text);
 
-        // Generate noise
-        for ($i = 0; $i < $this->dotNoiseLevel; $i++) {
-            imagefilledellipse($image, mt_rand(0, $width), mt_rand(0, $height), 2, 2, $textColor);
+        // Add noise
+        for ($i = 0; $i < $noisePixels; $i++) {
+            imagesetpixel($image, mt_rand(0, $width), mt_rand(0, $height), $noiseColor);
         }
         for ($i = 0; $i < $this->lineNoiseLevel; $i++) {
             imageline(
@@ -426,106 +381,86 @@ class Question extends AbstractWord
             );
         }
 
-        // Generate inline image if there is no need to transform
-        if (!$this->transformImage) {
-            // Capture image output
-            ob_start();
-            imagepng($image);
-            imagedestroy($image);
-            $imageData = ob_get_contents();
-            ob_end_clean();
+        // Transform image
+        if ($this->getTransformImage()) {
+            $transformedImage = imagecreatetruecolor($width, $height);
+            $bgColor = imagecolorallocate($transformedImage, 255, 255, 255);
+            imagefilledrectangle($transformedImage, 0, 0, $width - 1, $height - 1, $bgColor);
 
-            $this->setInlineImage('data:image/png;base64,' . base64_encode($imageData));
-            return;
-        }
+            // Apply wave transforms
+            $freq1 = $this->randomFreq();
+            $freq2 = $this->randomFreq();
+            $freq3 = $this->randomFreq();
+            $freq4 = $this->randomFreq();
 
-        // Transformed image
-        $transformedImage = imagecreatetruecolor($width, $height);
-        $bgColor = imagecolorallocate($transformedImage, 255, 255, 255);
-        imagefilledrectangle($transformedImage, 0, 0, $width - 1, $height - 1, $bgColor);
+            $ph1 = $this->randomPhase();
+            $ph2 = $this->randomPhase();
+            $ph3 = $this->randomPhase();
+            $ph4 = $this->randomPhase();
 
-        // Apply wave transforms
-        $freq1 = $this->randomFreq();
-        $freq2 = $this->randomFreq();
-        $freq3 = $this->randomFreq();
-        $freq4 = $this->randomFreq();
+            $szx = $this->randomSize();
+            $szy = $this->randomSize();
 
-        $ph1 = $this->randomPhase();
-        $ph2 = $this->randomPhase();
-        $ph3 = $this->randomPhase();
-        $ph4 = $this->randomPhase();
+            for ($x = 0; $x < $width; $x++) {
+                for ($y = 0; $y < $height; $y++) {
+                    $sx = $x + (sin($x*$freq1 + $ph1) + sin($y*$freq3 + $ph3)) * $szx;
+                    $sy = $y + (sin($x*$freq2 + $ph2) + sin($y*$freq4 + $ph4)) * $szy;
 
-        $szx = $this->randomSize();
-        $szy = $this->randomSize();
+                    if ($sx < 0 || $sy < 0 || $sx >= $width - 1 || $sy >= $height - 1) {
+                        continue;
+                    } else {
+                        $color   = (imagecolorat($image, $sx, $sy) >> 16)         & 0xFF;
+                        $colorX  = (imagecolorat($image, $sx + 1, $sy) >> 16)     & 0xFF;
+                        $colorY  = (imagecolorat($image, $sx, $sy + 1) >> 16)     & 0xFF;
+                        $colorXY = (imagecolorat($image, $sx + 1, $sy + 1) >> 16) & 0xFF;
+                    }
 
-        for ($x = 0; $x < $width; $x++) {
-            for ($y = 0; $y < $height; $y++) {
-                $sx = $x + (sin($x*$freq1 + $ph1) + sin($y*$freq3 + $ph3)) * $szx;
-                $sy = $y + (sin($x*$freq2 + $ph2) + sin($y*$freq4 + $ph4)) * $szy;
+                    if ($color == 255 && $colorX == 255 && $colorY == 255 && $colorXY == 255) {
+                        // ignore background
+                        continue;
+                    } elseif ($color == 0 && $colorX == 0 && $colorY == 0 && $colorXY == 0) {
+                        // transfer inside of the image as-is
+                        $newcolor = 0;
+                    } else {
+                        // do anti-aliasing for border items
+                        $fracX  = $sx - floor($sx);
+                        $fracY  = $sy - floor($sy);
+                        $fracX1 = 1 - $fracX;
+                        $fracY1 = 1 - $fracY;
 
-                if ($sx < 0 || $sy < 0 || $sx >= $width - 1 || $sy >= $height - 1) {
-                    continue;
-                } else {
-                    $color   = (imagecolorat($image, $sx, $sy) >> 16)         & 0xFF;
-                    $colorX  = (imagecolorat($image, $sx + 1, $sy) >> 16)     & 0xFF;
-                    $colorY  = (imagecolorat($image, $sx, $sy + 1) >> 16)     & 0xFF;
-                    $colorXY = (imagecolorat($image, $sx + 1, $sy + 1) >> 16) & 0xFF;
+                        $newcolor = $color   * $fracX1 * $fracY1
+                                  + $colorX  * $fracX  * $fracY1
+                                  + $colorY  * $fracX1 * $fracY
+                                  + $colorXY * $fracX  * $fracY;
+                    }
+
+                    imagesetpixel(
+                        $transformedImage, $x, $y, imagecolorallocate($transformedImage, $newcolor, $newcolor, $newcolor)
+                    );
                 }
-
-                if ($color == 255 && $colorX == 255 && $colorY == 255 && $colorXY == 255) {
-                    // ignore background
-                    continue;
-                } elseif ($color == 0 && $colorX == 0 && $colorY == 0 && $colorXY == 0) {
-                    // transfer inside of the image as-is
-                    $newcolor = 0;
-                } else {
-                    // do anti-aliasing for border items
-                    $fracX  = $sx - floor($sx);
-                    $fracY  = $sy - floor($sy);
-                    $fracX1 = 1 - $fracX;
-                    $fracY1 = 1 - $fracY;
-
-                    $newcolor = $color   * $fracX1 * $fracY1
-                              + $colorX  * $fracX  * $fracY1
-                              + $colorY  * $fracX1 * $fracY
-                              + $colorXY * $fracX  * $fracY;
-                }
-
-                imagesetpixel(
-                    $transformedImage, $x, $y, imagecolorallocate($transformedImage, $newcolor, $newcolor, $newcolor)
-                );
             }
-        }
-
-        // Generate noise for transformed image
-        for ($i = 0; $i < $this->dotNoiseLevel; $i++) {
-            imagefilledellipse($transformedImage, mt_rand(0, $width), mt_rand(0, $height), 2, 2, $textColor);
-        }
-        for ($i = 0; $i < $this->lineNoiseLevel; $i++) {
-            imageline(
-                $transformedImage,
-                mt_rand(0, $width), mt_rand(0, $height),
-                mt_rand(0, $width), mt_rand(0, $height),
-                $textColor
-            );
-        }
+        } // end transform image
 
         // Capture image output
         ob_start();
-        imagepng($transformedImage);
+        if ($this->getTransformImage()) {
+            imagepng($transformedImage);
+            imagedestroy($transformedImage);
+        } else {
+            imagepng($image);
+        }
         imagedestroy($image);
-        imagedestroy($transformedImage);
         $imageData = ob_get_contents();
         ob_end_clean();
 
-        $this->setInlineImage('data:image/png;base64,' . base64_encode($imageData));
+        return 'data:image/png;base64,' . base64_encode($imageData);
     }
 
     /**
      * Validate the question
      *
      * Exact code as \Zend\Captcha\AbstractWord::isValid() except for last part
-     * where it checks against the answer instead of the word/question
+     * where it checks against the answer instead of the word/question.
      *
      * @see    \Zend\Validator\ValidatorInterface::isValid()
      * @param  mixed $value
@@ -561,7 +496,7 @@ class Question extends AbstractWord
         }
 
         $this->id = $value['id'];
-        if ($input !== $this->getAnswer()) {
+        if (!$this->getService()->verify($input)) {
             $this->error(self::BAD_CAPTCHA);
             return false;
         }
