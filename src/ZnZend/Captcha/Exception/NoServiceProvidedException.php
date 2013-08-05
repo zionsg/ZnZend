bbdf5a62fc5f45b05a96b6be81118b613ca3f970
Zion Ng <zion@intzone.com>
2013-08-05 16:42:36 +0800

    Create NoServiceProvidedException.php

---
+++ src/ZnZend/Captcha/Exception/NoServiceProvidedException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * ZnZend
+ *
+ * @author Zion Ng <zion@intzone.com>
+ * @link   http://github.com/zionsg/ZnZend for canonical source repository
+ */
+
+namespace ZnZend\Captcha\Exception;
+
+class NoServiceProvidedException extends InvalidArgumentException
+{}
