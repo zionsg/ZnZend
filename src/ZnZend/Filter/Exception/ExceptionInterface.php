77a2472ee7cee5824fa6d09296c3f394b4fded98
Zion Ng <zion@intzone.com>
2013-08-20 11:39:43 +0800

    Create ExceptionInterface.php

---
+++ src/ZnZend/Filter/Exception/ExceptionInterface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * ZnZend
+ *
+ * @author Zion Ng <zion@intzone.com>
+ * @link   http://github.com/zionsg/ZnZend for canonical source repository
+ */
+
+namespace ZnZend\Filter\Exception;
+
+interface ExceptionInterface
+{
+}
