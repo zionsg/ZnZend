5099b2e3fa4b17113369268da3157c43f4e680b1
Zion Ng <zion@intzone.com>
2013-08-21 11:56:17 +0800

    Create RuntimeException.php

---
+++ src/ZnZend/Crypt/Exception/RuntimeException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * ZnZend
+ *
+ * @author Zion Ng <zion@intzone.com>
+ * @link   http://github.com/zionsg/ZnZend for canonical source repository
+ */
+
+namespace ZnZend\Crypt\Exception;
+
+class RuntimeException extends \RuntimeException implements ExceptionInterface
+{}
