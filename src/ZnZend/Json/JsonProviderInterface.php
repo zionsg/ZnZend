0b157b4b4380a5bfc28862554f1af120a0c1b247
Zion Ng <zion@intzone.com>
2014-02-21 09:26:38 +0800

    Create JsonProviderInterface.php

---
+++ src/ZnZend/Json/JsonProviderInterface.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * ZnZend
+ *
+ * @author Zion Ng <zion@intzone.com>
+ * @link   http://github.com/zionsg/ZnZend for canonical source repository
+ */
+
+namespace ZnZend\Json;
+
+/**
+ * Interface to provide JSON representation of object
+ *
+ * @see Zend\Json::encode() which looks for a toJson() method in the object first
+ *      before using json_encode()
+ */
+interface JsonProviderInterface
+{
+    /**
+     * Return JSON representation of object
+     *
+     * json_encode() encodes all public properties of an object.
+     * This method controls how the object is converted to JSON especially when
+     * its properties are protected and accessed only via getters.
+     * A subset of properties or additional info may be added in this way.
+     *
+     * @return string
+     */
+    public function toJson();
+}
