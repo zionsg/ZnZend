c71a0d49478cc19eb177ab7995d61c5993742bac
Ng Kitt Horng, Zion <zion@intzone.com>
2014-06-19 01:33:23 +0800

    Listen for exceptions as well

--- src/ZnZend/Listener/LogListener.php
+++ src/ZnZend/Listener/LogListener.php
@@ -13,12 +13,13 @@ use Zend\EventManager\EventManagerInterface;
 use Zend\EventManager\AbstractListenerAggregate;
 use Zend\EventManager\ListenerAggregateInterface;
 use Zend\Log\LoggerInterface;
+use Zend\Mvc\MvcEvent;
 
 /**
- * A simple listener to listen to logging events
+ * A simple listener to listen to logging events and exceptions
  *
  * This is used to show sample code in Module.php on how to set up a log listener.
- * This can also be easily extended by overriding the log() method.
+ * This can also be easily extended by overriding the log() and logException() methods.
  */
 class LogListener extends AbstractListenerAggregate
 {
@@ -27,7 +28,7 @@ class LogListener extends AbstractListenerAggregate
      *
      * @var array
      */
-    protected $events = array('emerg', 'alert', 'crit', 'err', 'warn', 'notice', 'info', 'debug');
+    protected $logEvents = array('emerg', 'alert', 'crit', 'err', 'warn', 'notice', 'info', 'debug');
 
     /**
      * Logger
@@ -55,17 +56,24 @@ class LogListener extends AbstractListenerAggregate
      * eg. when the following code is run in a controller:
      *     $this->getEventManager()->trigger('log', $this, array('param' => 'value'));
      *
+     * Listens for exceptions as well as Zend\Log\Logger::registerExceptionHandler() does not seem to work.
+     *
      * @param EventManagerInterface $events
      * @return void
      */
     public function attach(EventManagerInterface $events)
     {
         $sharedEvents      = $events->getSharedManager(); // must use shared manager else it will not work
-        $this->listeners[] = $sharedEvents->attach('*', $this->events, array($this, 'log'));
+        $this->listeners[] = $sharedEvents->attach('*', $this->logEvents, array($this, 'log'));
+        $this->listeners[] = $sharedEvents->attach(
+            'Zend\Mvc\Application',
+            MvcEvent::EVENT_DISPATCH_ERROR,
+            array($this, 'logException')
+        );
     }
 
     /**
-     * Write event to log
+     * Write log event
      *
      * Method must be public due to callback in attach().
      *
@@ -75,11 +83,39 @@ class LogListener extends AbstractListenerAggregate
     public function log(EventInterface $e)
     {
         $logLevel = $e->getName();
-        if (null == $this->logger || !in_array($logLevel, $this->events)) {
+        if (null == $this->logger || !in_array($logLevel, $this->logEvents)) {
             return;
         }
 
         $params = $e->getParams();
         $this->logger->{$logLevel}(sprintf('%s: %s', $logLevel, json_encode($params)));
+    }
+
+    /**
+     * Log exception
+     *
+     * Method must be public due to callback in attach().
+     *
+     * @param  EventInterface $e
+     * @return void
+     */
+    public function logException(EventInterface $e)
+    {
+        $exception = $e->getResult()->exception;
+        if (!$exception) {
+            return;
+        }
+
+        $trace = $exception->getTraceAsString();
+        $messages = array();
+        $i = 1;
+        do {
+            $messages[] = $i++ . ': ' . $exception->getMessage();
+        } while ($exception = $exception->getPrevious());
+
+        $logMessage = "Exception:\n" . implode("\n", $messages)
+                    . "\n\nTrace:\n" . $trace;
+
+        $this->logger->crit($logMessage); // passing $e->getParams() as 2nd argument will hang the application
     }
 }
