e3b22a1d86ba34fd01cc52dce3aeadce195cca28
Ng Kitt Horng, Zion <zion@intzone.com>
2015-05-18 11:55:26 +0800

    Create Version.php

---
+++ src/ZnZend/Version/Version.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * ZnZend
+ *
+ * @author Zion Ng <zion@intzone.com>
+ * @link   http://github.com/zionsg/ZnZend for canonical source repository
+ */
+
+namespace ZnZend\Version;
+
+/**
+ * Class to store and retrieve the version of ZnZend
+ */
+final class Version
+{
+    /**
+     * ZnZend version identification
+     */
+    const VERSION = '0.1.0';
+}
